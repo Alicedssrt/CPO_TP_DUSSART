e207db8d830047d065e75f5cead391ba0ae245e3
Alice <alicedussart34@gmail.com>
2024-09-27 15:29:43 +0200

    Création de l'exercice 3 du TP1, création d'une boucle while pour trouver une valeure aléatoire entre 0 et 100

---
+++ TP1_guessMyNumber_DUSSART/src/tp1_guessmynumber_dussart/TP1_guessMyNumber_DUSSART.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_guessmynumber_dussart;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alice
+ */
+public class TP1_guessMyNumber_DUSSART {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int nb;
+    Random generateurAleat = new Random();
+    int n = generateurAleat.nextInt(100);
+    System.out.println(n);
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Entrer un nombre entre 0 et 100:");
+    nb =sc.nextInt();
+    if (n>nb){
+        
+    }
+    }
+
+
+    }
+    
+
